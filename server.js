a295017925603f535b103505d84ff9dfdb414349
Dennis Mårtensson <dennismartensson@gmail.com>
2012-07-24 12:45:08 -0400

    fixes to core functions in server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var io = require('socket.io').listen(8000);
 
-io.sockets.on('onSync', function (socket) {
+io.sockets.on('onSync', function (socket, Peer) {
+	rtc.addPeer(Peer);
 
+	if(rtc.getPeers()){
+		socket.emit('addPeers', { peers: rtc.getPeers()});
+	}
 });
 
-io.sockets.on('onSyncEnd', function (socket) {
-
+io.sockets.on('onSyncEnd', function (socket, Peer) {
+	rtc.removePeer(Peer);x
 });
