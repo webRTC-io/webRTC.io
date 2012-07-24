bfc8c03241f53e4df608bdad843015ed80d93787
Dennis Mårtensson <dennismartensson@gmail.com>
2012-07-24 16:12:39 -0400

    creating rtc moduel and server start

--- rtc.js
+++ rtc.js
@@ -1,15 +1,50 @@
-var Peers = []
+var rtc = {
 
-function getPeers() {
-    return Peers;
-}
+	var connections = [],
 
-function addPeer(peer) {
-    Peers.push(peer);
-}
+	addPeer: function addPeer(peer, socket) {
+		var connection = {
+			id: socket.id;
+			socket: socket;
+			peer: peer;
+		};
+		connections.push(connection);
+	},
 
-function removePeer(peer) {
-    Peers.splice(Peers.indexOf(peer),1);
-}
+	removePeer: function removePeer(socket) {
+		for(var i=0; i<connections.length; i++) {
+			
+			if(connections[i].id === socket.id){
+				connections.splice(i, 1);
+			}
+		}
+	},
 
+	getPeers: function getPeers() {
+		var peers = [];
 
+		for(con in connections) {
+			peers.push(con.peer);
+		}
+
+		return peers;
+	},
+
+	getFromPeer: function getFromPeer(peer) {
+
+		for(con in connections) {
+			if(con.peer === peer){
+				return con;
+			}
+		}
+	},
+
+	getFromSocket: function getFromSocket(socket) {
+
+		for(con in connections) {
+			if(con.socket === socket){
+				return con;
+			}
+		}
+	}
+};
--- server.js
+++ server.js
@@ -1,14 +1,64 @@
 var io = require('socket.io').listen(8000);
 var rtc = require('./rtc');
 
-io.sockets.on('sync', function (socket, Peer) {
-	rtc.addPeer(Peer);
 
-	if(rtc.getPeers()){
-		socket.emit('add peers', { peers: rtc.getPeers()});
+//SYNC KEEP TRACK OF PEERS
+io.sockets.on('sync', function(socket, peer) {
+	console.log("sync received");
+
+	rtc.addPeer(socket, peer);
+
+	var peers = rtc.getPeers();
+	
+	if (peers) {
+		socket.emit('add peers', {
+			peers: peers
+		});
 	}
 });
 
-io.sockets.on('onSyncEnd', function (socket, Peer) {
-	rtc.removePeer(Peer);
+io.sockets.on('sync end', function(socket) {
+	rtc.removePeer(socket);
 });
+
+
+//CONNECT PEERS
+socket.on('ice candidate', function(data) {
+	console.log("ice candidate received");
+	socket.broadcast.emit('receive ice candidate', data);
+});
+
+socket.on('send offer', function(data) {
+	console.log("offer received");
+	socket.broadcast.emit('receive offer', data);
+});
+
+socket.on('send answer', function(data) {
+	console.log("answer received");
+	socket.broadcast.emit('receive answer', data);
+});
+
+
+//CONNECT PEER
+socket.on('ice candidate peer', function(data, peer) {
+	console.log("ice candidate received");
+
+	var peerSocket = rtc.getSocketFromPeer(peer);
+
+	peerSocket.emit('receive ice candidate', data);
+});
+
+socket.on('send offer peer', function(data, peer) {
+	console.log("offer received");
+
+	var peerSocket = rtc.getSocketFromPeer(peer);
+	peerSocket.emit('receive offer', data);
+});
+
+socket.on('send answer peer', function(data, peer) {
+	console.log("answer received");
+
+	var peerSocket = rtc.getSocketFromPeer(peer);
+	peerSocket.emit('receive answer', data);
+});
+
