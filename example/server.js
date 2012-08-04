0f9c69762359b2e6315f6c505f2c46bb01e34364
Dennis Mårtensson <dennismartensson@gmail.com>
2012-08-04 17:19:05 -0400

    added new callback style API

--- example/server.js
+++ example/server.js
@@ -1,17 +1,7 @@
-var io = require('../lib/webrtc.io').listen(8001);
-var colors = {};
+var rtc = require('../lib/webrtc.io')
+rtc.listen(8001);
 
-io.sockets.on('connection', function(socket) {
-  console.log("connection received");
-
-  colors[socket.id] = Math.floor(Math.random()* 0xFFFFFF)
-  socket.on('chat msg', function(msg) {
-    console.log("chat received");
-    
-    socket.broadcast.emit('receive chat msg', {
-      msg: msg,
-      color: colors[socket.id]
-    });
-  });
+rtc.on('connection', function() {
+  console.log("------------------------->connection received");
 });
 
