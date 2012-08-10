8a0cc7f97276bff0c71436e6e3772ffac9200fdc
Dennis Mårtensson <dennismartensson@gmail.com>
2012-08-10 14:44:20 -0400

    remove example

--- example/server.js
+++
@@ -1,9 +0,0 @@
-//SERVER
-var webRTC = require('../lib/webrtc.io').listen(8000);
-
-console.log('webRTC');
-console.log(webRTC);
-
-webRTC.rtc.on('connection', function(){
-    console.log('connection');
-});
